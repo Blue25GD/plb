96e312d393aad271bf817123c7e743ee5034d4b9
Alexandru Popescu <alexandru.popescu.fr@icloud.com>
2025-03-19 20:53:41 +0100

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+cp .env.example .env
+sed -i 's/changeme/$(openssl rand -base64 32 | tr -d /=+ | cut -c -32)/g' .env
 docker compose up -d
 cd api && npm install && cd ..
 cd frontend && npm install && cd ..
@@ -5,5 +7,5 @@ cd frontend && npm run build && cd ..
 npm install -g pm2
 # wait a few seconds for the database to be ready
 echo "Waiting for the database to be ready..."
-sleep 10
+sleep 25
 node bin/executeMigrations.js

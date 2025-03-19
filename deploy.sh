744f59d452c8e517478b49888913c58b634bc132
Alexandru Popescu <alexandru.popescu.fr@icloud.com>
2025-03-19 21:02:19 +0100

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cp sample.env .env
-sed -i 's/changeme/$(openssl rand -base64 32 | tr -d /=+ | cut -c -32)/g' .env
+sed -i "s/changeme/$(openssl rand -hex 16)/" .env
 docker compose up -d
 cd api && npm install && cd ..
 cd frontend && npm install && cd ..
@@ -8,4 +8,14 @@ npm install -g pm2
 # wait a few seconds for the database to be ready
 echo "Waiting for the database to be ready..."
 sleep 25
-node bin/executeMigrations.js
+cd api && node bin/executeMigrations.js && cd ..
+cd api && chmod +x start.sh && ./start.sh && cd ..
+cd frontend/dist || exit 1
+touch .htaccess
+echo "RewriteEngine On" > .htaccess
+echo "RewriteBase /" >> .htaccess
+echo "RewriteRule ^index\.html$ - [L]" >> .htaccess
+echo "RewriteCond %{REQUEST_FILENAME} !-f" >> .htaccess
+echo "RewriteCond %{REQUEST_FILENAME} !-d" >> .htaccess
+echo "RewriteRule . /index.html [L]" >> .htaccess
+cd ..
